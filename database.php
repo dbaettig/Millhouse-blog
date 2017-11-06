d07d9415afe4397dc379ea86d2d024ff7bddab66
dbaettig <daniela.baettig@hotmail.com>
2017-11-06 15:40:22 +0100

    edited register

--- database.php
+++ database.php
@@ -1,8 +1,7 @@
 <?php
 
-
 $pdo = new PDO(
-"mysql:host=localhost:dbname=projektarbete;charset=utf8",
-"root",
-"root"
+    "mysql:host=localhost;dbname=projektarbete;charset=utf8",
+    "root",
+    "root"
 );
