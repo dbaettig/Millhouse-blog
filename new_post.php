04fb0c1c141ba4c5a485db40e0c29f31a3820a14
omigibson <oumie.gibba@gmail.com>
2017-11-07 13:15:42 +0100

    Nån jävla konflikt igen

--- new_post.php
+++ new_post.php
@@ -1,5 +1,5 @@
 <form action="post_form.php" method="POST">
-	<input type= "text">
+	<input type="text" name="title" placeholder="title">
 	<input type= "text">
 	<input type= "text">
 	<input type= "text">
