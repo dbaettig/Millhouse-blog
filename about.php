860e96c6c05e3a91601f264b2c91d000c861ced5
omigibson <oumie.gibba@gmail.com>
2017-11-09 12:54:05 +0100

    Grund

--- about.php
+++ about.php
@@ -2,12 +2,36 @@
 require 'header.php';
 ?>
 
-<img>
+<style>
+	.about_wrapper {
+		width: 700px;
+		margin: 0px auto;
+	}
+	
+	.about_image_wrapper {
+		width: 80%;
+		margin: 0px auto;
+	}
+	
+	.about_text_wrapper {
+		width: 70%;
+		margin: 0px auto;
+	}
+</style>
+
+<div class="about_wrapper">
+<div class="about_image_wrapper">
+<img class= "about_image" src="img/watch3.jpeg">
+</div>
+
+<div class="about_text_wrapper">
 <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
 <br/>
 Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. 
 <br/>
 Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+</div>
+</div>
 
 <?php
 require 'footer.php';
