0275a209f41233f458fa8e7ec464c51e077e3a05
dbaettig <daniela.baettig@hotmail.com>
2017-11-09 11:31:20 +0100

    added footer style

--- footer.php
+++ footer.php
@@ -1,3 +1,20 @@
-<footer> </footer>
+<footer> 
+   <ul>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Shop</li>
+   </ul>
+    
+
+</footer>
 </body>
 </html>
+
+
+<div class="social">
+        <ul>
+            <li><a href="www.twitter.com"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://www.facebook.com/groups/509879569361835/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="www.instagram.com"><i class="fa fa-instagram"></i></a></li>
+        </ul>
+    </div>
