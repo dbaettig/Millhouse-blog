e93b3d0a6acd11a696ad395f6cc917498dd401bf
omigibson <oumie.gibba@gmail.com>
2017-11-10 13:40:13 +0100

    font awesome och wrapper

--- footer.php
+++ footer.php
@@ -1,33 +1,36 @@
+	</div> <!-- stänger wrappern-->
+</main>
+        
+<footer>
+	<ul>
+	<div class="about">
+		<li>
+		   <H3>About</H3>
+			<p>This is a blog my Millhouse.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam.</p>
+		</li>
+	</div>
 
-         <footer>
-        <ul>
-        <div class="about">
-            <li>
-               <H3>About</H3>
-                <p>This is a blog my Millhouse.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam.</p>
-            </li>
-        </div>
-        
-            <div class="contact">
-                <li>
-                   <H3>Contact</H3>
-                    <p>info@millhouse.com <br> 123-4567890 Wayway 2,<br><br> 12345 Place<br> COUNTRY</p>
-                </li>
-            </div>
-        
-                <div class="shop">
-                    <li> <H3>Shop</H3></li>
-                </div>
-       </ul>
-       
-       <div class="social">
-                        <ul>
-                            <li><a href="www.twitter.com"><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="https://www.facebook.com/groups/509879569361835/" target="_blank">
-                            <i class="fa fa-facebook"></i></a></li>
-                            <li><a href="www.instagram.com"><i class="fa fa-instagram"></i></a></li>
-                        </ul>
-        </div>
+		<div class="contact">
+			<li>
+			   <H3>Contact</H3>
+				<p>info@millhouse.com <br> 123-4567890 Wayway 2,<br><br> 12345 Place<br> COUNTRY</p>
+			</li>
+		</div>
+
+			<div class="shop">
+				<li> <H3>Shop</H3></li>
+			</div>
+   </ul>
+
+   <div class="social">
+		<ul>
+			<li><a href="www.twitter.com"><i class="fa fa-twitter"></i></a></li>
+			<li><a href="https://www.facebook.com/groups/509879569361835/" target="_blank">
+			<i class="fa fa-facebook"></i></a></li>
+			<li><a href="www.instagram.com"><i class="fa fa-instagram"></i></a></li>
+		</ul>
+	</div>
+	
 </footer>
 
 
