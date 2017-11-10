d8ead726918a43ad115d623862ffa8397df41f4c
omigibson <oumie.gibba@gmail.com>
2017-11-10 14:06:47 +0100

    färg på social icons och färg variabler i sass

--- footer.php
+++ footer.php
@@ -3,33 +3,27 @@
         
 <footer>
 	<ul>
-	<div class="about">
-		<li>
+		<li class="about list-item">
 		   <H3>About</H3>
 			<p>This is a blog my Millhouse.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam.</p>
 		</li>
-	</div>
 
-		<div class="contact">
-			<li>
-			   <H3>Contact</H3>
-				<p>info@millhouse.com <br> 123-4567890 Wayway 2,<br><br> 12345 Place<br> COUNTRY</p>
-			</li>
-		</div>
+		<li class="contact list-item">
+		   <H3>Contact</H3>
+			<p>info@millhouse.com <br> 123-4567890 Wayway 2,<br><br> 12345 Place<br> COUNTRY</p>
 
-			<div class="shop">
-				<li> <H3>Shop</H3></li>
-			</div>
+				<a href="www.twitter.com"><i class="fa fa-twitter"></i></a>
+				<a href="https://www.facebook.com/groups/509879569361835/" target="_blank">
+				<i class="fa fa-facebook"></i></a>
+				<a href="www.instagram.com"><i class="fa fa-instagram"></i></a>
+
+		</li>
+
+		<li class="shop list-item">
+			<H3>Shop</H3>
+		</li>
    </ul>
 
-   <div class="social">
-		<ul>
-			<li><a href="www.twitter.com"><i class="fa fa-twitter"></i></a></li>
-			<li><a href="https://www.facebook.com/groups/509879569361835/" target="_blank">
-			<i class="fa fa-facebook"></i></a></li>
-			<li><a href="www.instagram.com"><i class="fa fa-instagram"></i></a></li>
-		</ul>
-	</div>
 	
 </footer>
 
