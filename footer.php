a1234a21d504f49cd9180cee42b9f00105d9155d
dbaettig <daniela.baettig@hotmail.com>
2017-11-10 10:09:38 +0100

    fixa bugg

--- footer.php
+++ footer.php
@@ -52,7 +52,8 @@
        <div class="social">
                         <ul>
                             <li><a href="www.twitter.com"><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="https://www.facebook.com/groups/509879569361835/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="https://www.facebook.com/groups/509879569361835/" target="_blank">
+                            <i class="fa fa-facebook"></i></a></li>
                             <li><a href="www.instagram.com"><i class="fa fa-instagram"></i></a></li>
                         </ul>
         </div>
