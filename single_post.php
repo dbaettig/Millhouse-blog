5d2866db1956a175680ba34bb3e8d428bb657d32
omigibson <oumie.gibba@gmail.com>
2017-11-14 09:52:11 +0100

    Jobbar med single post

--- single_post.php
+++ single_post.php
@@ -3,13 +3,40 @@ session_start();
 require "header.php";
 require "database.php";
 ?>
+	<div class="wrapper">
+		<?php
+	
+	$statement = $pdo->prepare("SELECT * FROM posts WHERE postID = :postid");
+	$statement->execute(array(
+		":postid" => $_GET["postid"]
+	));
+	$single_post = $statement->fetch(PDO::FETCH_ASSOC);
+		
+foreach($single_post as $blogpost) { 
+		var_dump($blogpost); ?>
+
+			<h2>
+				<?=$blogpost['title']?>
+			</h2>
+			</a>
+			<small>
+							By <?=  $blogpost['username'] ?> in
+								<?= $blogpost['category'] ?> 
+								<?= $blogpost['created'] ?>
+						</small>
+	</div>
+	<p>
+		<?= $blogpost['post'] ?>
+	</p>
+	</div>
+	</div>
+	</div>
+	<?php }
+?>
 
 
 
-
-
-
-
-<?php
+	</div>
+	<?php
 require "footer.php";
 ?>
