732ab0dcea794a682a49988924839278288078d3
omigibson <oumie.gibba@gmail.com>
2017-11-14 14:00:48 +0100

    Fixed comment function

--- single_post.php
+++ single_post.php
@@ -9,10 +9,10 @@ require "database.php";
 	$statement = $pdo->prepare("SELECT * 
 	FROM posts 
 	JOIN users ON posts.userID = users.id 
-	WHERE posts.postID = :postid 
+	WHERE posts.postID = :postID 
 	");
 	$statement->execute(array(
-		":postid" => $_GET["postid"]
+		":postID" => $_GET["postID"]
 	));
 	$single_post = $statement->fetchAll(PDO::FETCH_ASSOC);
 		
@@ -23,18 +23,55 @@ foreach($single_post as $blogpost) {
 				<h2>
 					<?=$blogpost['title']?>
 				</h2>
-				</a>
+				<img src="<?= $blogpost['image'] ?>">
 				<small>
 							By <?=  $blogpost['username'] ?> in
 								<?= $blogpost['category'] ?> 
 								<?= $blogpost['created'] ?>
 						</small>
+				<p>
+					<?= $blogpost['post'] ?>
+				</p>
 			</div>
+			<?php }
+		?>
+			<div class="container">
+
+				<h3>Leave a comment</h3>
+				<form action="comment_form.php" method="POST">
+					<textarea name="comment" placeholder="Write your comment..." rows="6"></textarea>
+					<br /><input type="text" name="name" placeholder="Name">
+					<br/>
+					<input type="text" name="email" placeholder="Email">
+					<br/>
+					<input type="hidden" name="postID" value="<?=$_GET['postID']?>">
+
+					<input type="submit" name="submit" value="Publish">
+				</form>
+			</div>
+	
+	<br/>
+	<h3>Comments</h3>
+	<?php
+$statement = $pdo->prepare("SELECT * FROM comments  
+	WHERE postID = :postID");
+				   
+	$statement->execute(array(
+		":postID" => $_GET["postID"]
+	));
+	$comments = $statement->fetchAll(PDO::FETCH_ASSOC);
+				   
+				   foreach($comments as $comment) { 
+ ?>
+		<div class="blogpost">
+
+			<h4> By <?=  $comment['name']; ?> </h4>
+			<small> <?= $comment['created']; ?> </small>
 			<p>
-				<?= $blogpost['post'] ?>
+				<?= $comment['comment'] ?>
 			</p>
-	</div>
-</div>
-	<?php }
+		</div>
+		</div>
+		<?php }
 require "footer.php";
 ?>
