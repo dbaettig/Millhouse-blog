defbca36ef6b0d8bceb6c45b684e8cf08222a250
omigibson <oumie.gibba@gmail.com>
2017-11-15 14:23:40 +0100

    Fixed mobile styling

--- footer.php
+++ footer.php
@@ -4,12 +4,12 @@
 	<div class="footer-inner">
 		<ul>
 			<li class="footer-inner__info about">
-			   <H3>About</H3>
+			   <H4>About</H4>
 				<p>This is a blog my Millhouse.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam.</p>
 			</li>
 
 			<li class="footer-inner__info contact">
-			   <H3>Contact</H3>
+			   <H4>Contact</H4>
 				<p>info@millhouse.com <br> 123-4567890 Wayway 2,<br><br> 12345 Place<br> COUNTRY</p>
 
 					<a href="www.twitter.com"><i class="fa fa-twitter"></i></a>
@@ -20,7 +20,7 @@
 			</li>
 
 			<li class="footer-inner__info shop">
-				<H3>Shop</H3>
+				<H4>Shop</H4>
 			</li>
 	   </ul>
 	</div>
