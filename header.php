4e3882a1488c59f9f0a6ce923a9527bc8dc0c251
omigibson <oumie.gibba@gmail.com>
2017-11-15 15:31:48 +0100

    Fixed tablet styling

--- header.php
+++ header.php
@@ -59,15 +59,15 @@
 				</div>
 			</a>
 
-		<nav class="navbar navbar-expand-lg  navbar-dark bg-dark justify-content-end">
+		<nav class="navbar navbar-expand-sm navbar-dark bg-dark justify-content-end">
 			<a class="navbar-brand mobile" href="#">Navbar</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				  </button>
 			<div class="collapse navbar-collapse justify-content-center" id="navbarNav">
 				<ul class="navbar-nav">
-					<li class="nav-item active">
-						<a class="nav-link" href="index.php">Blog <span class="sr-only">(current)</span></a>
+					<li class="nav-item">
+						<a class="nav-link" href="index.php">Blog</a>
 					</li>
 					<li class="nav-item">
 						<a class="nav-link" href="about.php">About</a>
