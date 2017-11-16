ecb7ad3a3cc9e99339b807c52a6dae555fde7d88
lisasundberg <6d:b7:47:1a:d1:2a:bf:ea:19:11:30:9b:5a:d3:d7:a1>
2017-11-16 18:12:53 +0100

    Create image resize functionality

--- post_form.php
+++ post_form.php
@@ -26,4 +26,42 @@ $new_post->execute(array(
 	":category" => $_POST['category']
 ));
 	
+
+//Create image resize function
+function resize_image($file, $new_width, $new_height) {
+	
+  $info = getimagesize($file);
+  $width = $info[0]; //first value in $info array is width
+  $height = $info[1]; //second is height
+
+  // Save file as variable in PHP, copy the content of the file to $source: 
+  // imagecreatefromjpeg() returns an image identifier representing 
+  // the image obtained from the given filename.
+  
+  $source = imagecreatefromjpeg($file);
+  // create temporary destination variable with new sizes: 
+  // imagecreatetruecolor() returns an image identifier representing 
+  // a black image of the specified size.
+
+  $destination = imagecreatetruecolor($new_width, $new_height);
+  
+  // copy the original source to the new resized $destination
+  imagecopyresampled($destination, $source, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+  
+  // return the newly created and resized file
+  return $destination;
+}
+
+/***************************
+ *        USE CASE         *
+ ***************************/
+
+//Create the new resized image
+//first argument is file to convert
+//second is width
+//third is height
+$resized_image = resize_image("img/" . $filename, 1920, 1080);
+//Save the image to disk
+imagejpeg($resized_image, "resized/" . $filename, 100);
+
 header("Location:index.php");
