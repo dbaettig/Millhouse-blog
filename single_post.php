f18fe634f24780790dd2721d0052842916ed0252
lisasundberg <6d:b7:47:1a:d1:2a:bf:ea:19:11:30:9b:5a:d3:d7:a1>
2017-11-17 13:28:26 +0100

    Add logged in comment function to master branch

--- single_post.php
+++ single_post.php
@@ -20,17 +20,15 @@ foreach($single_post as $blogpost) {
  ?>
 			<div class="blogpost">
 
-				<h2>
-					<?=$blogpost['title']?>
-				</h2>
+				<h2><?=$blogpost['title']?></h2>
 				<div class="blogpost__image">
 					<img class="blogpost__image" src="<?= $blogpost['image'] ?>">
 				</div>
 				<small>
-							By <?=  $blogpost['username'] ?> in
-								<?= $blogpost['category'] ?> 
-								<?= $blogpost['created'] ?>
-						</small>
+					By <?=  $blogpost['username'] ?> in
+						<?= $blogpost['category'] ?> 
+						<?= $blogpost['created'] ?>
+				</small>
 				<p>
 					<?= $blogpost['post'] ?>
 				</p>
@@ -38,18 +36,32 @@ foreach($single_post as $blogpost) {
 			<?php }
 		?>
 			<br/>
-				<div class="comments_wrapper">
+				<div class="comments_wrapper container">
 
 				<h3 class="comments_header">Comments</h3>
-				<form action="comment_form.php" method="POST" class="comment_form">
-					<textarea name="comment" placeholder="Write your comment..." rows="6"></textarea>
-					<br /><input type="text" name="name" placeholder="Name">
-					<input type="text" name="email" placeholder="Email">
-					<br/>
-					<input type="hidden" name="postID" value="<?=$_GET['postID']?>">
+				
+				<?php if(!isset($_SESSION["user"])){ ?>
+					<!-- Comment form if not logged in -->
+					<form action="comment_form.php" method="POST" class="comment_form">
+						<textarea name="comment" placeholder="Write your comment..." rows="6"></textarea>
+						<br /><input type="text" name="name" placeholder="Name">
+						<input type="text" name="email" placeholder="Email">
+						<br/>
+						<input type="hidden" name="postID" value="<?=$_GET['postID']?>">
 
-					<input class="comment_submit" type="submit" name="submit" value="Post comment">
-				</form>
+						<input class="comment_submit" type="submit" name="submit" value="Post comment">
+					</form>
+					<?php } else { ?>
+						<!-- Comment form if logged in -->
+						<form action="comment_form.php" method="POST" class="comment_form">
+							<textarea name="comment" placeholder="Write your comment..." rows="6"></textarea>
+							<input type="hidden" name="postID" value=" <?=$_GET['postID']?> ">
+							<input type="hidden" name="userID" value=" <?=$_SESSION["user"]["id"]?> ">
+							<input type="hidden" name="name" value=" <?=$_SESSION["user"]["name"] ?>" >
+							<input type="hidden" name="email" value=" <?=$_SESSION["user"]["email"] ?>" >
+							<input class="comment_submit" type="submit" name="submit" value="Post comment">
+						</form>
+				<?php }	?>
 			</div>
 	
 	<br/>
@@ -62,7 +74,7 @@ $statement = $pdo->prepare("SELECT * FROM comments
 	));
 	$comments = $statement->fetchAll(PDO::FETCH_ASSOC);
 				   
-				   foreach($comments as $comment) { 
+	foreach($comments as $comment) { 
  ?>
 		<div class="comment">
 			<p> <?= $comment['comment'] ?> </p>
