30ba996ccdf68bd281d572f78f8a56511c4a605e
omigibson <oumie.gibba@gmail.com>
2017-11-21 15:29:07 +0100

    Fixed problem with comment function

--- profile.php
+++ profile.php
@@ -6,7 +6,7 @@ require "profile_database.php"
 ?>
 
 	<body>
-	<?php require 'navbar.php';?>
+		<?php require 'navbar.php';?>
 		<main>
 
 			<div class="profileWrapper">
@@ -65,40 +65,37 @@ require "profile_database.php"
 				<div class="latestInfo">
 					<div class="latestPosts">
 						<h3>Latest posts</h3>
-						
-		</div> <!--stänger numberInfo-->
-	</div> <!--stänger profileInfo-->
-</div> <!--stänger profileHeader-->
-</div> <!--stänger headerWrapper-->
+
+					</div>
+					<!--stänger numberInfo-->
+				</div>
+				<!--stänger profileInfo-->
+			</div>
+			<!--stänger profileHeader-->
+			</div>
+			<!--stänger headerWrapper-->
 
 
-						<?php
+			<?php
 	foreach($count2 as $c2) {
 	?>
 
-							<a href="single_post.php?postID=<?= $c2['postID'] ?>">
-								<p>
-									<?= $c2['title'] ?>
-								</p>
-							</a>
+				<a href="single_post.php?postID=<?= $c2['postID'] ?>">
+					<p>
+						<?= $c2['title'] ?>
+					</p>
+				</a>
 
-							<?php } ?>
-							<!--stänger loop-->
+				<?php } ?>
+				<!--stänger loop-->
 
-							</br>
+				</br>
 
-							<?php
-	foreach($count1 as $c1) {
-	?>
-
-	
-	<a href="index.php">See all posts (<?= $c1 ?>) <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
-
-
-								<a href="index.php">See all posts (<?= $c1 ?>) -></a>
-
-								<?php } ?>
-								<!--stänger loop-->
+				<?php
+	foreach($count1 as $c1) {	?>
+					<a href="index.php">See all posts (<?= $c1 ?>) <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+					<?php } ?>
+					<!--stänger loop-->
 
 					</div>
 					<!--stänger latestPosts-->
@@ -109,7 +106,6 @@ require "profile_database.php"
 						<?php
 	foreach($count4 as $c4) {
 	?>
-
 							<p>
 								<?= $c4['comment'] ?>
 							</p>
@@ -127,23 +123,20 @@ require "profile_database.php"
 	foreach($count3 as $c3) {
 	?>
 
-	
-	<a href="index.php">See all comments (<?= $c3 ?>) <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
 
-
-								<a href="index.php">See all comments (<?= $c3 ?>) -></a>
+								<a href="index.php">See all comments (<?= $c3 ?>) <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
 
 								<?php } ?>
 								<!--stänger loop-->
 
 					</div>
 					<!--stänger latestComments-->
-				</div>
-				<!--stänger latestInfo-->
+					</div>
+					<!--stänger latestInfo-->
 
-			</div>
-			<!--profileWrapper-->
+					</div>
+					<!--profileWrapper-->
 
-			<?php
+					<?php
 require 'footer.php';
 ?>
