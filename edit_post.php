012c05ec5718f0252de9ca577d3c046088864fd4
lisasundberg <6d:b7:47:1a:d1:2a:bf:ea:19:11:30:9b:5a:d3:d7:a1>
2017-11-22 13:23:26 +0100

    Fetch category from database when updating

--- edit_post.php
+++ edit_post.php
@@ -30,11 +30,11 @@ foreach($single_post as $blogpost) { ?>
 				<small style="text-align:left;">JPEG, Recommended file size 1000px x 564px.</small><br><br>
 
 				<select name="category" value="<?= $blogpost['category']; ?>">
-					<option value="category">Choose category...</option>
-					<option value="news">News</option>
-					<option value="style">Style</option>
-					<option value="interior">Interior</option>
-					<option value="featured">Featured</option>
+					<option value="category" <?php if($blogpost['category'] == 'category' ) { echo "selected='selected'"; }?> >Choose category...</option>
+					<option value="news" <?php if($blogpost['category'] == 'news' ) { echo "selected='selected'"; }?> >News</option>
+					<option value="style" <?php if($blogpost['category'] == 'style' ) { echo "selected='selected'"; }?> >Style</option>
+					<option value="interior" <?php if($blogpost['category'] == 'interior' ) { echo "selected='selected'"; }?>>Interior</option>
+					<option value="featured" <?php if($blogpost['category'] == 'featured' ) { echo "selected='selected'"; }?>>Featured</option>
 				</select>
 				<br><br>
 				<input type="submit" name="submit" value="Update">
