fb01ce22a45d405236fd1cae5534004e1e2c1554
omigibson <oumie.gibba@gmail.com>
2017-11-24 15:21:29 +0100

    Profile pic upload almost done

---
+++ profilepic_form.php
@@ -0,0 +1,21 @@
+<?php 
+session_start();
+require("database.php");
+
+$path = $_FILES["uploaded_file"]["tmp_name"];
+$filename = $_FILES["uploaded_file"]["name"];
+
+move_uploaded_file($path, "img/" . $filename);
+
+$profilepic = $pdo->prepare(
+	"UPDATE users 
+	SET profilepic = :profilepic
+	WHERE id = :userID"
+);
+
+$profilepic->execute(array(
+	":profilepic" => "img/" . $filename,
+	"userID" => $_SESSION['user']['id']
+));
+
+var_dump($_FILES);
