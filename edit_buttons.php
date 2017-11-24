df21b35c8c729cc2fea5e215af47041ce9b9cfc8
lisasundberg <6d:b7:47:1a:d1:2a:bf:ea:19:11:30:9b:5a:d3:d7:a1>
2017-11-24 12:21:10 +0100

    Styling profile and buttons

--- edit_buttons.php
+++ edit_buttons.php
@@ -2,10 +2,10 @@
 if (isset($_SESSION['user'])) {
     if (($_SESSION['user']['id']) == ($blogpost['userID'])) {  ?>
 
-    <button class="button_edit">
+    <button class="button_edit button_small button_turquoise">
 		<a href="edit_post.php?postID=<?= $blogpost['postID'] ?>">Edit Post</a>
 	</button>
-    <button class="button_delete">
+    <button class="button_delete button_small button_grey">
 		<a href="delete_post.php?postID=<?= $blogpost['postID'] ?>">Delete Post</a>
 	</button>
 <?php }
