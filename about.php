4017de32f29801225407dce8cb63e77de9098a1a
lisasundberg <6d:b7:47:1a:d1:2a:bf:ea:19:11:30:9b:5a:d3:d7:a1>
2017-11-27 13:58:05 +0100

    Add p-tags to about

--- about.php
+++ about.php
@@ -12,14 +12,10 @@ require 'partials/head.php';
 				</figure>
 
 				<div class="about__bodytext">
-					<p> Millhouse is a wholesale company that sells clothes, accessories and smaller interior design items to hundreds of fashion and lifestyle stores.
-						<br/>
-						<br/> The company was founded 60 years ago by Millhouse Simpson. His idea was that men and women everywhere should have the possibility to be welldresses at a reasonable price.
-						<br/>
-						<br/> Millhouse Simpsons successful business revolutionized the clothing industry and his thinking still affects many fashion campanies today.
-						<br/>
-						<br/> In the fall of 2017 Millhouse started our own webshop, selling our products directly to the consumers for the first time.
-					</p>
+					<p> Millhouse is a wholesale company that sells clothes, accessories and smaller interior design items to hundreds of fashion and lifestyle stores.</p>
+					<p>The company was founded 60 years ago by Millhouse Simpson. His idea was that men and women everywhere should have the possibility to be welldresses at a reasonable price.</p>
+					<p>Millhouse Simpsons successful business revolutionized the clothing industry and his thinking still affects many fashion campanies today.</p>
+					<p>In the fall of 2017 Millhouse started our own webshop, selling our products directly to the consumers for the first time.</p>
 				</div>
 			</article>
 
