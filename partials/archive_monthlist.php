781dcc91aee17489d310b212cf8fc2e994837a39
lisasundberg <6d:b7:47:1a:d1:2a:bf:ea:19:11:30:9b:5a:d3:d7:a1>
2017-12-06 15:19:05 +0100

    Add link in footer  to separate archive page

--- partials/archive_monthlist.php
+++ partials/archive_monthlist.php
@@ -1,9 +1,13 @@
 <?php 
-foreach ($archive_monthlist as $month) { ?>
-	<p>
-		<?php 
-			$date = date_create($month['created']);			  
-			echo date_format($date, 'F' . ' ' . 'Y');
-		?> 
-	</p>
-<?php } ?>
+foreach ($archive_monthlist as $month) {
+		$date = date_create($month['created']);
+		$month_year = date_format($date, 'F' . ' ' . 'Y');
+		?>
+		<p>
+			<a href="archive.php?<?= urlencode($month_year)?>">
+				<?= $month_year ?>
+			</a>
+		</p>
+<?php } 
+
+?>
