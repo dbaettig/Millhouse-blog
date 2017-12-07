bf0f0ff4e4e71c4c74cc72156acc0c5ccaab1fb2
lisasundberg <6d:b7:47:1a:d1:2a:bf:ea:19:11:30:9b:5a:d3:d7:a1>
2017-12-07 11:28:51 +0100

    Separate logic and presentation for archive

--- archive.php
+++ archive.php
@@ -22,20 +22,7 @@ require 'partials/database.php';
 			
 			<ul>
 			<?php
-				$statement = $pdo->prepare("
-				SELECT postID, title, post, category, created
-				FROM posts
-				WHERE monthname(created) = :month AND year(created) = :year
-				ORDER BY created DESC
-				");
-				
-				//GROUP BY MONTH(created)
-				$statement->execute(array(
-					":month" => $month,
-					":year" => $year
-				));
-				$posts_per_month = $statement->fetchAll(PDO::FETCH_ASSOC);
-	
+				require 'logic/archive_db.php';
 				foreach($posts_per_month as $post_single_month) { ?>
 					<li class="post">
 						<a href="single_post.php?postID=<?= $post_single_month['postID'] ?>">
