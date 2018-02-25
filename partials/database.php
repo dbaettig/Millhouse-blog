fc9329b02a026225f01088be1a19440a45f78778
omigibson <oumie.gibba@gmail.com>
2018-02-25 12:55:05 +0100

    Push group project to my own github account

--- partials/database.php
+++ partials/database.php
@@ -1,12 +1,9 @@
 <?php
 $pdo = new PDO(
-    "mysql:host=localhost;dbname=projektarbete;charset=utf8",
+   /* "mysql:host=localhost;dbname=projektarbete;charset=utf8",
     "root",
-    "root");
-
-/*
-$pdo = new PDO(
-    "mysql:host=omigibba.se;dbname=omigibba_projektarbete;charset=utf8",
+    "root");*/
+	
+	"mysql:host=omigibba.se;dbname=omigibba_projektarbete;charset=utf8",
     "omigibba_omig",
     "al5kA999");
-*/
